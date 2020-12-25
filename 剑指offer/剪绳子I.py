6716c472b28c21efcd88f58982d343e78c1be3eb
yuetao <806518802@qq.com>
2020-12-25 16:19:58 +0800

    剑指offer-剪绳子

--- 剑指offer/剪绳子I.py
+++ 剑指offer/剪绳子I.py
@@ -7,7 +7,9 @@
 # @Software: PyCharm
 # @desc    :
 
-#TODO
+"""
+dp[i] = dp[i-3] * 3
+"""
 
 
 class Solution(object):
@@ -19,16 +21,26 @@ class Solution(object):
         if n < 2:
             return 0
         dp = [float("-inf")] * (n+1)
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        if n == 4:
+            return 4
+        if n == 5:
+            return 6
+        if n == 6:
+            return 9
         dp[2] = 1
         dp[3] = 2
-        for i in range(3, n+1):
-            cur_status = []
-            length = i
-            while length > 1:
+        dp[4] = 4
+        dp[5] = 6
+        dp[6] = 9
+        for i in range(7, n+1):
+            dp[i] = 3 * dp[i-3]
+        return dp[n]
 
-
-
-if __name__ == "__mian__":
+if __name__ == "__main__":
     solve = Solution()
     n = int(input())
     result = solve.cuttingRope(n)
